7e691cb19e4abf54aaf9f59d006eca512d39a762
Phate1989 <goldstein.ryan@gmail.com>
2019-01-07 10:50:55 -0500

    Create ITglue_FIleShares.ps1

---
+++ ITglue_FIleShares.ps1
@@ -0,0 +1,6 @@
+﻿import-module itglueapi
+Add-ITGlueBaseURI -base_uri https://api.itglue.com
+Add-ITGlueAPIKey -Api_Key ITG.f9bd9adfc93f66ecff04789ea8613192.-59ev8wEaEE7MdcPO2P80CivswCL8b9uDZFp077ZDr2k1Y37boR6N9VBm4GSDGtf
+
+
+Get-ITGlueGroups
