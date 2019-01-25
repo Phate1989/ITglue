0f31a91f4b02ee4d1ae3ecad80ab75aaabed77e2
Phate1989 <goldstein.ryan@gmail.com>
2019-01-25 07:58:59 -0500

    Update ITglue_FIleShares.ps1

--- ITglue_FIleShares.ps1
+++ ITglue_FIleShares.ps1
@@ -1,6 +1,6 @@
 ﻿import-module itglueapi
 Add-ITGlueBaseURI -base_uri https://api.itglue.com
-Add-ITGlueAPIKey -Api_Key ITG.f9bd9adfc93f66ecff04789ea8613192.-59ev8wEaEE7MdcPO2P80CivswCL8b9uDZFp077ZDr2k1Y37boR6N9VBm4GSDGtf
+Add-ITGlueAPIKey -Api_Key ITG.bd9adfc93f66ecff04789ea8613192.-59ev8wEaEE7MdcPO
 $FormatEnumerationLimit = -1
 
 #Get-ITGlueFlexibleAssetTypes | convertto-json | out-file C:\Scripts\ExportTempData\asset.json 
